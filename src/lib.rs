3baaf707ae14c48e8303837ac27a74bb5bf0410b
Erik Rose <erik.w.rose@gmail.com>
2024-01-08 13:56:27 -0800

    update: docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
+#![warn(missing_docs)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
+//! An error-handling library for bubbling up errors in library code.
+
+/// The `error` module holds aliases for `Result` and `Error`.
 pub mod error;
 
+/// The `prelude` module exports library types intended for public use.
 pub mod prelude {
     pub use crate::error::{Clean, Bandage};
 }
