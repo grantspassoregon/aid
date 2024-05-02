677bd714c9d863c59b90a31ad4936e9bcd0e62f6
Erik Rose <erik.w.rose@gmail.com>
2024-05-01 17:09:53 -0700

    winit event loop error added

--- src/error.rs
+++ src/error.rs
@@ -33,10 +33,22 @@ pub enum Bandage {
         /// The `value` field returns messages on missing parameters in the builder function.
         value: Vec<String>,
     },
+    /// The `Utf8` variant converts a `std::str::Utf8Error`.
+    #[error("Utf8 error: {0}")]
+    Utf8(#[from] std::str::Utf8Error),
+    /// The `Hint` variant encloses a message with an error.
+    #[error("Hint: {0}")]
+    Hint(String),
+    /// The `Unknown` variant is a catch-all error variant for library operations.
+    #[error("Unexpected error.")]
+    Unknown,
+    /// The `BadIcon` results from a failed import of an icon image file into the Dioxus desktop
+    /// app.
     #[cfg(feature = "icon")]
     #[cfg_attr(docsrs, doc(cfg(feature = "icon")))]
     #[error("Icon loading error.")]
     BadIcon(#[from] dioxus_desktop::tao::window::BadIcon),
+    /// The `Bin` variant indicates a failure during binary encoding in crate `bincode`.
     #[cfg(feature = "bin")]
     #[cfg_attr(docsrs, doc(cfg(feature = "bin")))]
     #[error("Could not serialize to binary.")]
@@ -46,18 +58,23 @@ pub enum Bandage {
     #[cfg_attr(docsrs, doc(cfg(feature = "byte")))]
     #[error("Byte conversion failed.")]
     Byte(#[from] byte_unit::ParseError),
+    /// The `Csv` variant converts an error returned by the `csv` crate. Currently indicates
+    /// failure to generate a csv writer.
     #[cfg(feature = "csvs")]
     #[cfg_attr(docsrs, doc(cfg(feature = "csvs")))]
     #[error("Could not make CSV writer.")]
     Csv(#[from] csv::Error),
+    /// The `Http` variant converts an error from the `reqwest` crate.
     #[cfg(feature = "req")]
     #[cfg_attr(docsrs, doc(cfg(feature = "req")))]
     #[error("HTTP request error.")]
     Http(#[from] reqwest::Error),
+    /// The `Image` variant converts an error from the `image` crate.
     #[cfg(feature = "img")]
     #[cfg_attr(docsrs, doc(cfg(feature = "img")))]
     #[error("Image processing error.")]
     Image(#[from] image::error::ImageError),
+    /// The `Oauth2` variant converts an error from the `oauth2` crate.
     #[cfg(feature = "oauth")]
     #[cfg_attr(docsrs, doc(cfg(feature = "oauth")))]
     #[error("Oauth2 error.")]
@@ -68,27 +85,79 @@ pub enum Bandage {
     #[cfg_attr(docsrs, doc(cfg(feature = "serial")))]
     #[error("Deserialize error.")]
     Serialize(#[from] serde::de::value::Error),
+    /// The `SerdeJson` variant converts an error from the `serde_json` crate.
     #[cfg(feature = "serial")]
     #[cfg_attr(docsrs, doc(cfg(feature = "serial")))]
-    #[error("Deserialization error.")]
-    Conversion(#[from] serde_json::Error),
-    #[cfg(feature = "uuids")]
-    #[cfg_attr(docsrs, doc(cfg(feature = "uuids")))]
+    #[error("Deserialization error: {0}")]
+    SerdeJson(#[from] serde_json::Error),
+    /// The `Sqlx` variant converts a general error from the `sqlx` crate.
+    #[cfg(feature = "sql")]
+    #[cfg_attr(docsrs, doc(cfg(feature = "sql")))]
+    #[error("Sqlx command error: {0}")]
+    Sqlx(#[from] sqlx::Error),
+    /// The `Migrate` variant converts a migration error from the `sqlx` crate.
+    #[cfg(feature = "sql")]
+    #[cfg_attr(docsrs, doc(cfg(feature = "sql")))]
+    #[error("Sqlx migration error: {0}")]
+    Migrate(#[from] sqlx::migrate::MigrateError),
+    /// The `Uuid` variant converts an error from the `uuid` crate.
+    #[cfg(feature = "id")]
+    #[cfg_attr(docsrs, doc(cfg(feature = "id")))]
     #[error("Uuid conversion failed.")]
     Uuid(#[from] uuid::Error),
+    /// The `Url` variant converts an error from the `url` crate.
     #[cfg(feature = "urls")]
     #[cfg_attr(docsrs, doc(cfg(feature = "urls")))]
     #[error("Parse error.")]
     Url(#[from] url::ParseError),
+    /// The `BitMap` variant converts an error from the `plotters_bitmap` crate.
     #[cfg(feature = "plot")]
     #[cfg_attr(docsrs, doc(cfg(feature = "plot")))]
     #[error("Plotting backend error.")]
     BitMap(#[from] plotters_bitmap::BitMapBackendError),
+    /// The `Plot` variant converts an error from the `plotters` crate.
     #[cfg(feature = "plot")]
     #[cfg_attr(docsrs, doc(cfg(feature = "plot")))]
     #[error("Plotting drawing error.")]
     Plot(#[from] plotters::drawing::DrawingAreaErrorKind<plotters_bitmap::BitMapBackendError>),
-    /// The `Unknown` variant is a catch-all error variant for library operations.
-    #[error("Unexpected error.")]
-    Unknown,
+    /// The `GeoJson` variant converts an error from the `geojson` crate.
+    #[cfg(feature = "gis")]
+    #[cfg_attr(docsrs, doc(cfg(feature = "gis")))]
+    #[error("Error reading geojson file: {0}")]
+    GeoJson(#[from] geojson::Error),
+    /// The `TraceInit` variant converts an error from the `tracing_subscriber` crate.
+    #[cfg(feature = "trace")]
+    #[cfg_attr(docsrs, doc(cfg(feature = "trace")))]
+    #[error("Problem initializing subscriber: {0}")]
+    TraceInit(#[from] tracing_subscriber::util::TryInitError),
+    /// The `Axum` variants converts an *axum::Error* from the `axum` crate.
+    #[cfg(feature = "route")]
+    #[cfg_attr(docsrs, doc(cfg(feature = "route")))]
+    #[error("Axum error: {0}")]
+    Axum(#[from] axum::Error),
+    /// The `AxumHttp` variant converts an axum::http error from the `axum` crate.
+    #[cfg(feature = "route")]
+    #[cfg_attr(docsrs, doc(cfg(feature = "route")))]
+    #[error("Axum http error: {0}")]
+    AxumHttp(#[from] axum::http::Error),
+    /// The `Hyper` variant converts a `hyper_util::error::Error` from the `hyper_util` crate.
+    #[cfg(feature = "hype")]
+    #[cfg_attr(docsrs, doc(cfg(feature = "hype")))]
+    #[error("Legacy client error: {0}")]
+    Hyper(#[from] hyper::Error),
+    /// The `HyperUtil` variant converts a `hyper_util::client::legacy::Error` from the `hyper_util` crate.
+    #[cfg(feature = "hype")]
+    #[cfg_attr(docsrs, doc(cfg(feature = "hype")))]
+    #[error("Legacy client error: {0}")]
+    HyperUtil(#[from] hyper_util::client::legacy::Error),
+    /// The `HyperUtil` variant converts a `hyper_util::client::legacy::Error` from the `hyper_util` crate.
+    #[cfg(feature = "gis")]
+    #[cfg_attr(docsrs, doc(cfg(feature = "gis")))]
+    #[error("Shapefile error: {0}")]
+    Shapefile(#[from] shapefile::Error),
+    /// The `EventLoop` variant converts a [`winit::error::EventLoopError`] from the `winit` crate.
+    #[cfg(feature = "win")]
+    #[cfg_attr(docsrs, doc(cfg(feature = "win")))]
+    #[error("Winit event loop error: {0}")]
+    EventLoop(#[from] winit::error::EventLoopError)
 }
